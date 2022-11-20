db328211d55e406658f874f6ba21117217d4b5c0
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-11-20 16:24:52 +0100

    update main fetch
    fetch + siatka

---
+++ src/js/fetch-to-main.js
@@ -0,0 +1,37 @@
+<<<<<<< Updated upstream
+=======
+const API_KEY =`209b988e1e5a3c54f84bfbe290fdf3e2`
+getMovie = document.getElementById(`movie-list`)
+function fetchMovies(API_KEY){
+fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
+  .then((response) => response.json())
+  .then((data) => 
+  
+  start(data)
+  );
+}
+fetchMovies((API_KEY));
+
+function start(fetchMovies){
+    let moviez ="";
+    movies = fetchMovies;
+    movieIndex =movies.results;
+    console.log(movies.results);
+    for (i =0; i < movieIndex.length; i++){
+        moviez += `
+        <li> 
+        <div class="movie-container">
+        <img class="movie-image" src=https://image.tmdb.org/t/p/w500/${movieIndex[i].poster_path}>
+        <p class="movie-title">${movieIndex[i].name || movieIndex[i].title}</p>
+        </div>
+        </li>`
+        console.log(movieIndex[i].name || movieIndex[i].title);
+    }
+    getMovie.innerHTML =moviez;
+
+
+    }
+
+
+
+>>>>>>> Stashed changes
