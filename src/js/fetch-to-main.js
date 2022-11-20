b86176fcfaecf21f29a86a02b7743a00499dc02a
MaksymilianBilski <biger9@o2.pl>
2022-11-20 20:58:37 +0100

    Update fetch-to-main.js

--- src/js/fetch-to-main.js
+++ src/js/fetch-to-main.js
@@ -1,35 +1,29 @@
-const API_KEY =`209b988e1e5a3c54f84bfbe290fdf3e2`
-getMovie = document.getElementById(`movie-list`)
-function fetchMovies(API_KEY){
-fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
-  .then((response) => response.json())
-  .then((data) => 
-  
-  start(data)
-  );
+const API_KEY = `209b988e1e5a3c54f84bfbe290fdf3e2`;
+let getMovie = document.getElementById(`movie-list`);
+function fetchMovies(API_KEY) {
+  fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => start(data));
 }
-fetchMovies((API_KEY));
-let options="";
+fetchMovies(API_KEY);
+let options = '';
 
-function start(fetchMovies){
-    let moviez ="";
-    movies = fetchMovies;
-    movieIndex =movies.results;
-    console.log(movies.results);
-    for (i =0; i < movieIndex.length; i++){
-        moviez += `
+function start(fetchMovies) {
+  let moviez = '';
+  movies = fetchMovies;
+  movieIndex = movies.results;
+  console.log(movies.results);
+  for (i = 0; i < movieIndex.length; i++) {
+    moviez += `
         <li> 
         <div class="movie-container">
-        <img class="movie-image" src=https://image.tmdb.org/t/p/w500/${movieIndex[i].poster_path}>
+        <img class="movie-image" src=https://image.tmdb.org/t/p/w500/${
+          movieIndex[i].poster_path
+        }>
         <p class="movie-title">${movieIndex[i].name || movieIndex[i].title}</p>
         </div>
-        </li>`
-        console.log(movieIndex[i].name || movieIndex[i].title);
-    }
-    getMovie.innerHTML =moviez;
-
-
-    }
-
-
-
+        </li>`;
+    console.log(movieIndex[i].name || movieIndex[i].title);
+  }
+  getMovie.innerHTML = moviez;
+}
