e6471f76e782d364eb82b30b2e0444d59578e29e
MaksymilianBilski <biger9@o2.pl>
2022-11-26 22:36:47 +0100

    bugfix

--- src/js/add-to-queue-watched.js
+++ src/js/add-to-queue-watched.js
@@ -1,18 +1,21 @@
-const queueBtn = document.querySelector(".btn-queue");
-const watchedBtn = document.querySelector(".btn-watched");
-let queueArray=[];
-let watchedArray=[]; 
+const queueBtn = document.querySelector('.btn-queue');
+const watchedBtn = document.querySelector('.btn-watched');
 
-watchedBtn.addEventListener('click',() =>{  
-    let movieID=101010;
-    watchedArray.push(movieID);
-console.log("dupaduap");
-    
-    localStorage.setItem('wached', JSON.stringify(watchedArray))})
+let queueArray = [];
+let watchedArray = [];
 
+// obrałem to w funkcje, bo przed otworzeniem modala, button jeszcze nie istnieje i wywala błąd
+function test() {
+  if (watchedBtn !== null) {
+    watchedBtn.addEventListener('click', () => {
+      let movieID = 101010;
+      watchedArray.push(movieID);
+      console.log('dupaduap');
 
-
-  
-
-function addToWatched() {
+      localStorage.setItem('wached', JSON.stringify(watchedArray));
+    });
+  } else return;
 }
+test();
+
+function addToWatched() {}
