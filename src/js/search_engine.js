640a3ad7c0bec900322d302eceea352748b0a828
Konrad Włodarczyk <draco320@gmail.com>
2022-11-26 20:00:29 +0100

    v1

--- src/js/search_engine.js
+++ src/js/search_engine.js
@@ -9,34 +9,36 @@
 //   timeDifference,
 // } from './fetch-to-main';
 // import { Spinner } from 'spin.js';
+// import { opts } from './asynchronic-loader-opts';
 
-// const API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+// const API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
 // const input = document.getElementById('form-input');
 // const searchForm = document.getElementById('search-form');
-// let SEACRH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
+
+// let SEACRH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+// let totalSearchedPages
 
 // searchForm.addEventListener('submit', event => {
-//   // const spinner = new Spinner(opts).spin(getMovie);
-//   event.preventDefault();
-//   let searchValue = input.value.toLowerCase()
+//   const spinner = new Spinner(opts).spin(getMovie);
 //   if (getMovie.children.length > 0) {
 //     getMovie.innerHTML = '';
 //   }
+//   event.preventDefault();
+//   let searchValue = input.value.toLowerCase();
 //   if (searchValue === '') {
-//     fetchMovies(API_KEY)
+//     fetchMovies(API_KEY);
 //   }
-//   if (searchValue && searchValue !=='') {
-//     fetch(SEACRH_URL+searchValue)
-//     .then(data => {
-//       return data.json()
-//     })
-//     .then(movies => {
-//       start(movies)
-//     })
-//     searchValue=''
-//     // spinner.stop();
+//   if (searchValue && searchValue !== '') {
+//     fetch(SEACRH_URL + searchValue)
+//       .then(data => {
+//         return data.json();
+//       })
+//       .then(movies => {
+//         start(movies);
+//       });
+//     searchValue = '';
+//     spinner.stop();
 //   }
-  
 // });
 
 import { async } from 'regenerator-runtime';
@@ -51,29 +53,41 @@ import {
 } from './fetch-to-main';
 import { Spinner } from 'spin.js';
 import { opts } from './asynchronic-loader-opts';
+
 const API_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
-const input = document.getElementById('form-input');
+
+let SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+export let totalSearchedPages;
 const searchForm = document.getElementById('search-form');
-let SEACRH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
-searchForm.addEventListener('submit', event => {
-  const spinner = new Spinner(opts).spin(getMovie);
-  if (getMovie.children.length > 0) {
-    getMovie.innerHTML = '';
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  function search(page) {
+    const API_KEY = `209b988e1e5a3c54f84bfbe290fdf3e2`;
+    const input = document.getElementById('form-input');
+    const spinner = new Spinner(opts).spin(getMovie);
+    if (getMovie.children.length > 0) {
+      getMovie.innerHTML = '';
+    }
+    // event.preventDefault();
+    let searchValue = input.value.toLowerCase();
+    if (searchValue === '') {
+      fetchMovies(API_KEY);
+      search = false;
+    }
+    if (searchValue && searchValue !== '') {
+      fetch(SEARCH_URL + searchValue + page)
+        .then(data => {
+          return data.json();
+        })
+        .then(movies => {
+          start(movies);
+          totalSearchedPages = movies.total_pages;
+        });
+      searchValue = '';
+      spinner.stop();
+    }
   }
-  event.preventDefault();
-  let searchValue = input.value.toLowerCase();
-  if (searchValue === '') {
-    fetchMovies(API_KEY);
-  }
-  if (searchValue && searchValue !== '') {
-    fetch(SEACRH_URL + searchValue)
-      .then(data => {
-        return data.json();
-      })
-      .then(movies => {
-        start(movies);
-      });
-    searchValue = '';
-    spinner.stop();
-  }
+  search("");
 });
