1a1e7ae313f96f21b3f015bdbb1e56bfd19782f3
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-11-29 06:52:33 +0100

    update
    mentor notes

--- src/js/index-library.js
+++ src/js/index-library.js
@@ -2,7 +2,7 @@ import '../sass/main.scss';
 import './queue-watched-storage';
 import './modal-library';
 import './letDetails';
-
+import '../js/fetch-to-main'
 import '../js/queue-watched-storage';
 import '../js/modal-library';
 
